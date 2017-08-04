d6e1ee0f7c8090a2e4ca5d8ba3350f414d66b6fe
snitke <shawnnitke@gmail.com>
2017-08-04 13:32:22 -0500

    Make comments even BETTER

--- client/index.js
+++ client/index.js
@@ -1,13 +1,14 @@
-console.log("index.js loaded");
-
 var socket = io();
 
+// Capture the button click event
 $("button").click(ButtonClick);
 function ButtonClick() {
 
+  // Emit the button press for the server to intercept
   socket.emit("fromClientButtonPress");
 }
 
+// Look for the button press event coming from the server
 socket.on("fromServerButtonPress", ButtonEvent);
 function ButtonEvent() {
 
--- server/server.js
+++ server/server.js
@@ -6,17 +6,11 @@ const express = require("express")
 // Read in the client files
 app.use(express.static("client"));
 
-// http server on port 3000
+// Start http server using express
 http.listen(3000, ServerListening);
 function ServerListening() {
 
   console.log("Server listening (localhost:3000)...");
-}
-
-app.get("/", AppGet);
-function AppGet(req, res) {
-
-  res.sendFile("/srv/socketio/client/index.html");
 }
 
 // On connection, get the socket
